3ee0721b2213d9e9d26200af9feb5066ba2c80f8
Wallace <rj.wallacesilva@hotmail.com>
2021-10-27 20:47:07 -0300

    Instalação da biblioteca Fast Route.

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+# Carrega o autoload.
+require_once 'vendor/autoload.php';
+
+# Converte a URL em um caminho absoluto.
+$uri = $_SERVER['REQUEST_URI'];
+$selfUri = $_SERVER['PHP_SELF'];
+$uri = rawurldecode($uri);
+$base = \explode(basename(__FILE__), $selfUri);
+
+$baseUrlPath = implode('',$base);
+
+# Carega os arquivos básicos de inicialização da aplicação.
+require_once __DIR__ . '/src/Config/bootstrap.php';
